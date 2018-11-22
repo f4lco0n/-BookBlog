6827da8c91c122fb1c468b9776cd2229a87219b1
f4lco0n <pawel_sokol@o2.pl>
2018-11-22 17:25:43 +0100

    added cookies while logging

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from flask_login import current_user, login_user
 from flask_login import logout_user
 from app.models import User, Opinion, Book
 from flask_login import login_required
-from flask import request
+from flask import request, make_response
 from werkzeug.urls import url_parse
 from app import db
 from app.forms import RegistrationForm, AddBookForm
@@ -17,7 +17,6 @@ from sqlalchemy import and_
 def index():
     posts = Opinion.query.all()
 
-    
     return render_template('index.html', title='Home', posts=posts) 
 
 
@@ -32,11 +31,17 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
+        
+        userCookie = request.form['username']
+        resp = make_response(render_template('index.html'))
+        resp.set_cookie('user', userCookie)
         next_page = request.args.get('next')
+
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
+            return resp
         return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('login.html', title='Sign In', form=form)    
 
 
 @app.route('/show_books', methods=['GET', 'POST'])
@@ -50,7 +55,6 @@ def detail_book(title):
     book = Book.query.filter_by(title=title).first_or_404()
     result = db.session.query(Book.id, Opinion.body).filter(Opinion.book_id==book.id).filter(and_(Book.title == title)).all()
     return render_template('detail_book.html', book=book, result=result)
-#https://stackoverflow.com/questions/34688555/sqlalchemy-how-to-query-3-tables-with-association-table
 
 
 
@@ -59,7 +63,8 @@ def add_book():
     if current_user.is_authenticated:
         form = AddBookForm()
         if form.validate_on_submit():
-            added_book = Book(title=form.title.data, description=form.description.data)
+            added_book = Book(title=form.title.data, description=form.description.data,
+                            author=form.author.data, pages=form.pages.data)
             db.session.add(added_book)
             db.session.commit()
             flash('Book has been added')
@@ -73,7 +78,10 @@ def add_book():
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    resp = make_response(redirect('/index'))
+    resp.delete_cookie('user')
+    return resp
+    #return redirect(url_for('index'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
